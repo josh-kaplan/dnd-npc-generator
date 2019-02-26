1ad0f1828b969c409d2ab37511b54738099e0d5b
Michael Ferris <mike.ferris@hotmail.com>
2019-02-26 13:08:06 -0800

    Fix webpack build

--- webpack-production.config.js
+++ webpack-production.config.js
@@ -1,13 +1,3 @@
-module.exports = [
-	require("./make-webpack-config")({
-		// commonsChunk: true,
-		longTermCaching: true,
-		separateStylesheet: true,
-		minimize: true,
-		// devtool: "source-map",
-	}),
-	require("./make-webpack-config")({
-		minimize: true,
-		prerender: false,
-	})
-];
+module.exports = require("./make-webpack-config")({
+	minimize: true,
+});
